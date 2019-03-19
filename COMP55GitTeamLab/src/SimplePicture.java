b9f1f44449556deff646eabcca32547d14d8b436
Carl <carlobtinalla@yahoo.com>
2019-03-19 14:25:36 -0700

    Changed width to 200 added comment

--- COMP55GitTeamLab/src/SimplePicture.java
+++ COMP55GitTeamLab/src/SimplePicture.java
@@ -1,4 +1,3 @@
-
 // Tuesday
 // The Lazy Wolves
 // 3.20.2019
@@ -16,11 +15,7 @@ public class SimplePicture extends GraphicsProgram {
 	public void run() {
 		GImage robot = new GImage("robot.jpg", 200, 100);
 		add(robot);
-<<<<<<< HEAD
-		GLabel label = new GLabel("How I feel when using github hackslash", 150, 400);
-=======
-		GLabel label = new GLabel("How I feel when using github hackslash", 4638292, 400);
->>>>>>> branch 'master' of https://github.com/comp55-spr19/git-team-lab-the-lazy-wolves.git
+		GLabel label = new GLabel("How I feel when using github hackslash", 400, 400); // Carl Obtinalla
 		add(label);
 	}
 }
